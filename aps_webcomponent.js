5d5865c0cc0e1bf7de29c0282d2e5e7a7a5fdefa
AlfredWangTJ <92712829+AlfredWangTJ@users.noreply.github.com>
2023-03-14 14:40:24 +0800

    Update aps_webcomponent.js

--- aps_webcomponent.js
+++ aps_webcomponent.js
@@ -1,9 +1,49 @@
 (function () {
     let version = "1.0.0";
+    let tmpl = document.createElement('template');
+    tmpl.innerHTML = `
+        <p>Date Format</p>
+        <select id="select" style="width: 100%; padding: 3px 5px; font-family: "72" sans-serif;>
+            <option name="aps_format" value="">Automatic</option>
+            <option name="aps_format" value="YYYY-MM-dd">YYYY-MM-DD</option>
+            <option name="aps_format" value="MM/dd/YYYY">MM/DD/YYYY</option>
+            <option name="aps_format" value="dd.MM.YYYY">DD.MM.YYYY</option>
+        </select>
+        <p>Theme</p>
+        <input type="checkbox" id="theme" /><label for="checkbox">Use dark theme</label>
+        <p>Miscellaneous</p>
+        <input type="checkbox" id="range" /><label for="checkbox">Enable date range selection</label>
+    `;
 
     class DatePickerAps extends HTMLElement {
         constructor() {
             super();
+            this._shadowRoot = this.attachShadow({ mode: 'open' });
+            this._shadowRoot.appendChild(tmpl.content.cloneNode(true));
+            ["select", "theme", "range"].forEach(id =>
+                this._shadowRoot.getElementById(id).addEventListener("change", this._submit.bind(this)));
+        }
+
+        _submit(e) {
+            e.preventDefault();
+            this.dispatchEvent(new CustomEvent('propertiesChanged', {
+                detail: {
+                    properties: {
+                        format: this.format,
+                        darktheme: this.darktheme,
+                        enablerange: this.enablerange
+                    }
+                }
+            }));
+            return false;
+        }
+
+        get format() {
+            return this._shadowRoot.querySelector("option[name='aps_format']:checked").value;
+        }
+
+        set format(value) {
+            this._shadowRoot.querySelector("option[name='aps_format'][value='" + value + "']").checked = "checked";
         }
     }
 
